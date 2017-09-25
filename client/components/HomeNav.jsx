d610485f43cb2586fcc4d7987923ed8cb1a22a6d
harrison-symes <symeshjb@gmail.com>
2017-09-25 14:17:48 +1300

    hero setup, nav is coming together

--- client/components/HomeNav.jsx
+++ client/components/HomeNav.jsx
@@ -3,17 +3,12 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 const navbarStart = (auth) => (
   <div className="navbar-start">
-    <Link className="navbar-item is-active" to="/my/conversations">Conversations</Link>
-    {" | "}
-    <Link className="navbar-item" to="/my/cohorts/find/new">Find Cohort</Link>
-    {" | "}
-    <Link className="navbar-item" to="/my/learning">Learning</Link>
-    {" | "}
-    <Link className="navbar-item" to="/my/profile">Profile</Link>
-    {auth.user.is_admin==true && <span>
-      {" | "}
-      <Link className="navbar-item" to="/my/admin">Admin</Link>
-    </span>}
+    <Link className="navbar-item button is-inverted is-primary is-large" to="/my/conversations">Conversations</Link>
+    <Link className="navbar-item button is-inverted is-primary is-large" to="/my/cohorts/find/new">Find Cohort</Link>
+    <Link className="navbar-item button is-inverted is-primary is-large" to="/my/learning">Learning</Link>
+    <Link className="navbar-item button is-inverted is-primary is-large" to="/my/profile">Profile</Link>
+
+    {auth.user.is_admin==true && <Link className="navbar-item button is-inverted is-success is-large" to="/my/admin">Admin</Link>}
   </div>
 )
 
@@ -27,12 +22,11 @@ const navbarEnd = (isAuthenticated, showConfirmLogout, toggleLogout) => (
           <button className="button level-item" onClick={() => toggleLogout(true)}>Yes</button>
           <button className="button level-item" onClick={() => toggleLogout(false)}>No</button>
         </p>
-        : <Link className="navbar-item" to="/" onClick={() => toggleLogout(false)}>Logout</Link>
-      : <div>
-        <Link className="navbar-item" to="/login">Login</Link>
-        {" | "}
-        <Link className="navbar-item" to="/register">Register</Link>
-      </div>
+        : <Link className="navbar-item button is-danger is-inverted is-large" to="/" onClick={() => toggleLogout(false)}>Logout</Link>
+      : <span className="navbar-item">
+        <Link className="button is-primary is-inverted is-large" to="/login">Login</Link>
+        <Link className="button is-info is-inverted is-large" to="/register">Register</Link>
+      </span>
     }
   </div>
 )
@@ -57,19 +51,24 @@ export default class HomeNav extends React.Component {
     let {auth, logout} = this.props
     let {showConfirmLogout, burgerShow} = this.state
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigations">
-        <div className="navbar-brand">
-          <button onClick={this.burgerToggle} className={`button navbar-burger ${burgerShow ? "is-active" : " "}`} data-target="navMenu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-        <div className={`navbar-menu ${burgerShow ? "is-active" : " "}`} id="navMenu">
-          {auth.isAuthenticated && navbarStart(auth)}
-          {navbarEnd(auth.isAuthenticated, showConfirmLogout, this.toggleLogout)}
-        </div>
-      </nav>
+      <section className="section is-info">
+        <nav className="navbar" role="navigation" aria-label="main navigations">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="#">
+              <img style={{width: '20vw'}}src="http://bulma.io/images/bulma-logo.png" />
+            </a>
+            <div onClick={this.burgerToggle} className={`navbar-burger burger ${burgerShow ? "is-active" : " "}`} data-target="navMenu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div className={`navbar-menu is-info ${burgerShow ? "is-active" : " "}`} id="navMenu">
+            {auth.isAuthenticated && navbarStart(auth)}
+            {navbarEnd(auth.isAuthenticated, showConfirmLogout, this.toggleLogout)}
+          </div>
+        </nav>
+      </section>
     )
   }
 }

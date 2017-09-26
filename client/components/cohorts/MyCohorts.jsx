341b6eeaeb689c910691634ddd093f96bb20c64c
harrison-symes <symeshjb@gmail.com>
2017-09-26 14:27:55 +1300

    search bar for both my and find cohorts

--- client/components/cohorts/MyCohorts.jsx
+++ client/components/cohorts/MyCohorts.jsx
@@ -25,23 +25,23 @@ export default class MyCohorts extends React.Component {
   render() {
     let {cohorts} = this.props
     let {search} = this.state
-    let filtered = cohorts.filter(c => c.name.toLowerCase().includes(search))
+    let filtered = cohorts.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
     const renderCohort = (cohort, i) => <MyCohortSingle key={i} cohort={cohort} />
     return (
       <div className="container">
         <h1 className="title is-1">Cohorts</h1>
+        <input className={`input ${filtered.length == 0 ? "is-danger" : "is-primary"} ${search.length > 0 ? "is-focused" : "is-small"}`} type="text" name="search" placeholder="Search My Cohorts" onChange={this.updateSearch} value={search} />
         <hr />
         <div className="content columns">
-          <FindCohort />
           <div className="column">
             <h1 className="subtitle is-1">My Cohorts</h1>
-            <input className={`input ${filtered.length == 0 ? "is-danger" : "is-primary"} ${search.length > 0 ? "is-focused" : "is-small"}`} type="text" name="search" placeholder="Search My Cohorts" onChange={this.updateSearch} value={search} />
             <hr />
             {filtered.length > 0
               ? filtered.map(renderCohort)
               : <p className="tag is-danger is-large">No Matches</p>
             }
           </div>
+          <FindCohort search={search}/>
         </div>
       </div>
     )

00733f591bd27d05710fabe382f0f6353dd77dc0
harrison-symes <>
2017-09-28 16:09:18 +1300

    krang

--- client/components/learning/MyLearning.jsx
+++ client/components/learning/MyLearning.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default class MyLearning extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 0
+    }
+    this.addClick = this.addClick.bind(this)
+  }
+  addClick() {
+    this.setState({clicks: this.state.clicks + 1})
+    console.log(this.state.clicks);
+  }
   render() {
     return (
       <div className="columns">
         <div className="column">
           <h1 className="subtitle is-1">Review Learning</h1>
           <hr />
-          <div className="content">
-            <p className="tag is-large is-inverted">No Learning Objectives To Review</p>
+          <div onClick={this.addClick} className="content">
+            <p className="tag is-large is-inverted" >{this.state.clicks >= 10 ? "I HATE THE TURTLES!!!!" : "No Learning Objectives To Review"}</p>
+            {this.state.clicks >= 10 && <img src="https://vignette.wikia.nocookie.net/villains/images/1/13/Krang_80s.png/revision/latest?cb=20161124163608" />}
             <Link className="button is-success is-large" to="/my/learning/review">Review Past Learning</Link>
           </div>
         </div>

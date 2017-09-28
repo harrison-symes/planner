9b27cb146f3f5fa1b27cb1936ab6b650a98d101f
ushamacdonald <>
2017-09-28 14:40:48 +1300

    remove user_ids from objective suggestions

--- server/db/learning.js
+++ server/db/learning.js
@@ -9,4 +9,5 @@ module.exports = {
     .then(id => getLearningObjectiveById(db, id[0])),
   getObjectivesByUserIds: (db, user_ids) => db('learningObjectives')
     .whereIn('user_id', user_ids)
+    .select('learningObjectives.title', 'learningObjectives.id')
 }

77ec24aa49b33319d65b31a5201da684d1c8e7c8
harrison-symes <>
2017-09-29 12:29:11 +1300

    key for tag colours on learning review, and moment setup to show monday-sunday weeks

--- client/components/learning/LearningPlans.jsx
+++ client/components/learning/LearningPlans.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import moment from 'moment'
+
+function getMonday(d) {
+  d = new Date(d);
+  var day = d.getDay(),
+  diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+  return new Date(d.setDate(diff));
+}
+
+function getNextSunday (d) {
+  d = new Date(d)
+  var day = d.getDay()
+  var diff = d.getDate() + 6
+  return new Date(d.setDate(diff))
+}
 
 export default class LearningPlans extends React.Component {
   constructor(props) {
@@ -15,14 +30,22 @@ export default class LearningPlans extends React.Component {
   }
   render() {
     const {learningPlans} = this.props
+    console.log(getMonday(new Date()));
     const renderPlanPreview = (plan, i) => <div className={`button ${plan.is_reflected ? (plan.is_reviewed ? "is-success" : "is-warning") : "is-danger"}`} key={i}>
-      {plan.created_at}
+      {console.log(moment.unix(getMonday(new Date)), moment(plan.created_at))}
+      {moment(getMonday(plan.created_at)).format('MMM Do') + " - " +  moment(getNextSunday(getMonday(plan.created_at))).format('MMM Do')}
     </div>
     return (
       <div className="column">
         <h1 className="subtitle is-1">Review Learning</h1>
         <hr />
         <div className="content" onClick={this.addClick}>
+          <div className="columns">
+            <p className="tag is-small is-primary column">In Progress  </p>
+            <p className="tag is-small is-danger column">Needs Reflection</p>
+            <p className="tag is-small is-warning column">Reflected</p>
+            <p className="tag is-small is-success column">Reviewed + Reflected</p>
+          </div>
           {learningPlans.length != 0
             ? learningPlans.map(renderPlanPreview)
             : <p  className="tag is-large is-inverted" >{this.state.clicks >= 10 ? "I HATE THE TURTLES!!!!" : "No Learning Objectives To Review"}</p>

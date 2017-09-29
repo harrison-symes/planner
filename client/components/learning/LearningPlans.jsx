a75b4e6cc55b2e748fc815dad9cce5c88c39ff4b
harrison-symes <>
2017-09-29 15:15:37 +1300

    seeds for learning plans refined and shows your learning objective for current week

--- client/components/learning/LearningPlans.jsx
+++ client/components/learning/LearningPlans.jsx
@@ -3,20 +3,6 @@ import {Link} from 'react-router-dom'
 import moment from 'moment'
 import {getWeekSpan, getDayName} from '../../utils/moment'
 
-function getMonday(d) {
-  d = new Date(d);
-  var day = d.getDay(),
-  diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
-  return new Date(d.setDate(diff));
-}
-
-function getNextSunday (d) {
-  d = new Date(d)
-  var day = d.getDay()
-  var diff = d.getDate() + 6
-  return new Date(d.setDate(diff))
-}
-
 export default class LearningPlans extends React.Component {
   constructor(props) {
     super(props)
@@ -31,7 +17,6 @@ export default class LearningPlans extends React.Component {
   }
   render() {
     const {learningPlans} = this.props
-    console.log(getMonday(new Date()));
     const isThisWeek = (date) => getWeekSpan(date) == getWeekSpan(new Date())
     const renderPlanPreview = (plan, i) => <div className={`button ${isThisWeek(plan.created_at) ? "is-primary": (plan.is_reflected ? (plan.is_reviewed ? "is-success" : "is-warning") : "is-danger")}`} key={i}>
       <p>{getWeekSpan(plan.created_at)} ({getDayName(plan.created_at)}) </p>
@@ -40,19 +25,22 @@ export default class LearningPlans extends React.Component {
       <div className="column">
         <h1 className="subtitle is-1">Review Learning</h1>
         <hr />
-        <div className="content" onClick={this.addClick}>
-          <div className="columns">
-            <p className="tag is-small is-primary column">In Progress  </p>
-            <p className="tag is-small is-danger column">Needs Reflection</p>
-            <p className="tag is-small is-warning column">Reflected</p>
-            <p className="tag is-small is-success column">Reviewed + Reflected</p>
+        {learningPlans.length
+          ? <div className="content" onClick={this.addClick}>
+            <div className="columns">
+              <p className="tag is-small is-primary column">In Progress  </p>
+              <p className="tag is-small is-danger column">Needs Reflection</p>
+              <p className="tag is-small is-warning column">Reflected</p>
+              <p className="tag is-small is-success column">Reviewed + Reflected</p>
+            </div>
+            <div className="content">
+              <p className="subtitle is-3">This Week</p>
+              {learningPlans.filter(p => isThisWeek(p.created_at)).map(renderPlanPreview)}
+              <hr />
+            </div>
           </div>
-          {learningPlans.length != 0
-            ? learningPlans.map(renderPlanPreview)
-            : <p  className="tag is-large is-inverted" >{this.state.clicks >= 10 ? "I HATE THE TURTLES!!!!" : "No Learning Objectives To Review"}</p>
-          }
-        </div>
-
+          : <p  className="tag is-large is-inverted" >{this.state.clicks >= 10 ? "I HATE THE TURTLES!!!!" : "No Learning Objectives To Review"}</p>
+        }
           {this.state.clicks >= 10 && <img src="https://vignette.wikia.nocookie.net/villains/images/1/13/Krang_80s.png/revision/latest?cb=20161124163608" />}
           <Link className="button is-success is-large" to="/my/learning/review">Review Past Learning</Link>
       </div>

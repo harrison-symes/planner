a534c5f3f55534dc3b116f653ea7df0c7829fd0f
harrison-symes <>
2017-10-03 09:47:06 +1300

    get user by id depth test passing

--- test/server/db/users.test.js
+++ test/server/db/users.test.js
@@ -6,6 +6,32 @@ var setupDb = require('../setup-db')
 
 setupDb(test,createServer)
 
+var usersDb = require('../../../server/db/users')
+
 test('Users Db Tests Working', t => {
   t.pass()
 })
+
+//getUserById
+test.cb('getUserById returns the correct user without hash', t => {
+  const expected = {
+    user_id: 1,
+    user_name: 'symeshjb',
+    first_name: 'Harrison',
+    last_name: 'Symes',
+    'is_admin': 1,
+    about: 'I made dis'
+  }
+  usersDb.getUserById(t.context.db, expected.user_id)
+    .then(actual => {
+      t.false(actual.hasOwnProperty('hash'))
+      t.false(actual.hasOwnProperty('password'))
+      for (let key in expected) {
+        t.true(actual.hasOwnProperty(key))
+        t.is(actual[key], expected[key])
+      }
+      t.end()
+    })
+})
+
+//getUsersToInvite

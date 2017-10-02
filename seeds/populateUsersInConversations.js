a2fc17e1ee37100f9bf41dc0765d8409a55961fc
harrison-symes <>
2017-10-03 10:29:16 +1300

    depth testing for get user conversations

--- seeds/populateUsersInConversations.js
+++ seeds/populateUsersInConversations.js
@@ -8,7 +8,9 @@ exports.seed = function(knex, Promise) {
         {conversation_id: 1, user_id: 1},
         {conversation_id: 1, user_id: 2},
         {conversation_id: 2, user_id: 1},
-        {conversation_id: 2, user_id: 2}
+        {conversation_id: 2, user_id: 2},
+        {conversation_id: 3, user_id: 2},
+        {conversation_id: 3, user_id: 3}
       ]);
     });
 };
--- test/server/db/conversations.test.js
+++ test/server/db/conversations.test.js
@@ -6,6 +6,8 @@ var setupDb = require('../setup-db')
 
 setupDb(test,createServer)
 
+const conversationsDb = require('../../../server/db/conversations')
+
 test('Conversations Db Tests Working', t => {
   t.pass()
 })
@@ -13,6 +15,27 @@ test('Conversations Db Tests Working', t => {
 //CONVERSATIONS ---
 
 //getConversations
+test.cb('getConversations returns the correct converations for user 1', t => {
+  const expectedArr = [
+    {user_id: 1, conversation_id: 1, name: 'Harrison and Joshua'},
+    {user_id: 1, conversation_id: 2, name: "What's for Lunch"}
+  ]
+
+  const user_id = 1
+  conversationsDb.getConversations(t.context.db, user_id)
+    .then(actualArr => {
+      t.is(actualArr.length, expectedArr.length)
+      actualArr.forEach(actual => {
+        let expected = expectedArr.find(exp => exp.conversation_id === actual.conversation_id)
+        t.true(expected !== null)
+        for (let key in expected) {
+          t.true(actual.hasOwnProperty(key))
+          t.is(actual[key], expected[key])
+        }
+      })
+      t.end()
+    })
+})
 
 //getUsersInConversation
 

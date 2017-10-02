fb82677a2053af808770574383a59b93d6f24ba9
harrison-symes <>
2017-10-02 16:38:56 +1300

    get cohorts works

--- test/server/db/cohorts.test.js
+++ test/server/db/cohorts.test.js
@@ -6,6 +6,30 @@ var setupDb = require('../setup-db')
 
 setupDb(test,createServer)
 
+const cohortsDb = require('../../../server/db/cohorts')
+
 test('Cohorts Db Tests Working', t => {
   t.pass()
 })
+
+//getCohorts
+test.cb('Get Cohorts returns correct number of overall cohorts', t => {
+  var expected = 5
+   cohortsDb.getCohorts(t.context.db)
+    .then(actual => {
+      t.is(actual.length, expected)
+      t.end()
+    })
+    .catch(err => {
+      t.fail()
+      t.end()
+    })
+})
+
+//getCohort
+
+//joinCohort
+
+//getUserCohorts
+
+//usersInCohorts

3125f705764902ea472aef5df21e54d4450321b2
harrison-symes <>
2017-10-02 16:07:31 +1300

    testing user exists and get User By Name

--- test/server/db/auth.test.js
+++ test/server/db/auth.test.js
@@ -6,6 +6,56 @@ var setupDb = require('../setup-db')
 
 setupDb(test,createServer)
 
+var usersDb = require('../../../server/db/users')
+
 test('Auth Db Tests Working', t => {
   t.pass()
+})
+
+//userExists
+test.cb('User Exists for existing user', t => {
+   usersDb.userExists('symeshjb', t.context.db)
+    .then(exists => {
+      t.true(exists)
+      t.end()
+    })
+})
+
+test.cb('User Exists for non-existing user', t => {
+   usersDb.userExists('mrMemeMan', t.context.db)
+    .then(exists => {
+      t.false(exists)
+      t.end()
+    })
+    .catch(() => t.fail())
+})
+
+
+//createUser
+
+
+//getUserByName
+test.cb('Get User By Name for existing user', t => {
+   usersDb.getUserByName('symeshjb', t.context.db)
+    .then(user => {
+      t.true(user.hasOwnProperty('user_name'))
+      t.true(user.hasOwnProperty('id'))
+      t.true(user.hasOwnProperty('about'))
+      t.true(user.hasOwnProperty('first_name'))
+      t.true(user.hasOwnProperty('last_name'))
+      t.true(user.hasOwnProperty('hash'))
+      t.true(user.hasOwnProperty('is_private'))
+      t.true(user.hasOwnProperty('is_admin'))
+      t.end()
+    })
+    .catch(() => t.fail())
+})
+
+test.cb('Get User By Name for non-existing user', t => {
+   usersDb.getUserByName('mrMemeMan', t.context.db)
+    .then(user => {
+      t.true(!user)
+      t.end()
+    })
+    .catch(() => t.fail())
 })

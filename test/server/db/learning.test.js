031ecc40a6ea27b12cd4fceeeacd9c95fae1478a
harrison-symes <>
2017-10-04 09:36:44 +1300

    get objectives by user ids first test

--- test/server/db/learning.test.js
+++ test/server/db/learning.test.js
@@ -6,6 +6,72 @@ var setupDb = require('../setup-db')
 
 setupDb(test,createServer)
 
+const learningDb = require('../../../server/db/learning')
+
 test('Learning Db Tests Working', t => {
   t.pass()
 })
+
+
+//Objectives
+
+
+//getLearningObjectiveById
+test.cb('getLearningObjectiveById', t => {
+  const expected = {
+    title: 'Full Stack Project',
+    user_id: 1,
+    id: 1
+  }
+  learningDb.getLearningObjectiveById(t.context.db, expected.id)
+    .then(actual => {
+      t.true(actual != null)
+      for (let key in expected) {
+        t.true(actual.hasOwnProperty(key))
+        t.is(actual[key], expected[key])
+      }
+      t.end()
+    })
+})
+
+//getObjectivesByUserIds
+test.only.cb('getObjectivesByUserIds', t => {
+  const expectedArr = [
+    {title: 'Use Postgresql locally', id: 2},
+    {title: 'Vue.js', id: 3}
+  ]
+  const user_ids = [2, 3]
+  learningDb.getObjectivesByUserIds(t.context.db, user_ids)
+    .then(actualArr => {
+      actualArr.forEach(actual => {
+        let expected = expectedArr.find(exp => exp.id == actual.id)
+        t.true(expected != null)
+        for (let key in expected) {
+          t.true(actual.hasOwnProperty(key))
+          t.is(actual[key], expected[key])
+        }
+      })
+      t.end()
+    })
+})
+
+//getJoinedObjectivesByUserIds
+
+//getObjectivesByPlanId
+
+//insertLearningObjective
+
+//insertObjectivesArray
+
+
+
+
+
+
+//Plans
+
+//getLearningPlanById
+
+//getLearningPlansByUser
+
+//insertLeaningPlan
